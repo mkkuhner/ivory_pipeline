c41276914a8ecc22e5d193b1739aded2f3ba210d
jay <mkkuhner@uw.edu>
2021-05-27 14:15:30 -0700

    fixed up script to work with current 2021/05/27 codebase

--- src/laptop_setup_seizure.sh
+++ src/laptop_setup_seizure.sh
@@ -4,7 +4,7 @@
 #    the new directory that will be created to run the seizure analysis in
 # $2 is the path to the elephant pipeline directory
 # $3 is the path to the source of raw seizure data files
-# $4 is the path to a current scat2 executable
+# $4 is the absolute path to a current scat2 executable
 #
 # plus a directory "refdata" containing:
 #   REFELE_[Dbno]_known.txt, REFELE_[Dbno]_known_structure.txt_f
@@ -20,14 +20,12 @@ cd $1
 python3 prep_scat_data.py $1
 python3 make_eb_input.py REFELE_4.3_known_structure.txt_f REFELE_4.3_known.txt $1 dropoutrates_savannahfirst.txt
 Rscript ebscript.R
-python3 make_species_files.py $1 mapfile_161220 regionfile.v36.txt REFELE_4.3 notT
+python3 filter_hybrids.py $1 mapfile_161220 regionfile.v36.txt REFELE_4.3 notT
 
 if test -f "runfile_forest.sh"; then
   mkdir nforest
   cp $1_forest.txt nforest
   cp voronoi_runfile_forest.sh nforest
-  cp *_forest_fammatch_longformat.csv nforest
-  cp *_forest_fammatch_wide.csv nforest
   cp $4 nforest
   perl -p -i -e "s/SCAT2/scat2.4/" runfile_forest.sh
   python3 setupscatruns.py nforest runfile_forest.sh 1001
@@ -37,8 +35,6 @@ if test -f "runfile_savannah.sh"; then
   mkdir nsavannah
   cp $1_savannah.txt nsavannah
   cp voronoi_runfile_savannah.sh nsavannah
-  cp *_savannah_fammatch_longformat.csv nsavannah
-  cp *_savannah_fammatch_wide.csv nsavannah
   cp $4 nsavannah
   perl -p -i -e "s/SCAT2/scat2.4/" runfile_savannah.sh
   python3 setupscatruns.py nsavannah runfile_savannah.sh 2001
