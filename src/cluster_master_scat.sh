850fa13fc67d2e3abfea44f12037cc2858c8cd77
Mary Kuhner <mkkuhner@uw.edu>
2021-06-16 12:53:10 -0700

    Changes for use of the Klone cluster.

--- src/cluster_master_scat.sh
+++ src/cluster_master_scat.sh
@@ -2,17 +2,19 @@
 
 #SBATCH --job-name=UNAME                               # give a unique name
 #SBATCH --account=wasser
-#SBATCH --partition=wasser
+#SBATCH --partition=compute
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=4G
 #SBATCH --time=720:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=mkkuhner@uw.edu
-#SBATCH -o CIVscat.%j.out
-#SBATCH -e CIVscat.%j.err
+#SBATCH -o scat.%j.out
+#SBATCH -e scat.%j.err
 #SBATCH --chdir=LOCALPATH  # set local directory
 
 pwd; hostname; date;
+
+module load gcc/10.2.0
 
 /gscratch/wasser/mkkuhner/scat/SCAT2 -A 1 NUMIND -Z -S SEED -g MAPFILE DATAFILE REGIONFILE outputs 16 100 20 100
