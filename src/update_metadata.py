4192bfb6f71f75e0ffdb415a74b699510018364d
Mary Kuhner <mkkuhner@uw.edu>
2021-10-06 13:00:45 -0700

    Modifying update_metadata to deal correctly with previously seen seizures.

--- src/update_metadata.py
+++ src/update_metadata.py
@@ -1,13 +1,17 @@
 # update the seizure_metadata file with info from a new seizure
+# this program updates its input file IN PLACE
+# note that it takes both PREFIX and seizure name; the latter should
+# be in comma-separated format
+# it will silently rename previously seen samples
 
 import sys
 if len(sys.argv) != 4:
-  print("USAGE:  update_metadata.py metafile.tsv PREFIX_unknowns.txt seizure_name")
+  print("USAGE:  update_metadata.py metafile.tsv PREFIX seizure_name")
   print("WARNING:  this program updates the file IN PLACE.")
   exit(-1)
 
 metafile = sys.argv[1]
-seizurefile = sys.argv[2]
+seizurefile = sys.argv[2]+"_unknowns.txt"
 seizure_name = sys.argv[3]
 
 # read sample ids from new seizure
@@ -21,18 +25,18 @@ newsids = list(sids)
 # read old metadata file
 outlines = []
 for line in open(metafile,"r"):
-  saveline = line
+  outline = line
   line = line.rstrip().split("\t")
   if line[0].startswith("Seizure"):  continue
   sid = line[1]
   if sid in newsids:
-    print("Sample",sid,"already present in metadata file.")
-    print("Run aborting without change to metadata.")
-    exit(-1)
-  outlines.append(saveline)
+    # renaming an existing entry
+    outline = seizure_name + "\t" + sid + "\n"
+  outlines.append(outline)
 
 # add new sids to it
 for sid in newsids:
+  # adding a new entry
   outline = seizure_name + "\t" + sid + "\n"
   outlines.append(outline)
 
