2b0841c064d6720fceb72832deb23a01d00ba19f
Mary Kuhner <mkkuhner@uw.edu>
2023-03-15 14:23:05 -0700

    Debugging step0.

--- src/step0_setup.py
+++ src/step0_setup.py
@@ -16,26 +16,33 @@ import os
 
 if len(sys.argv) != 5:
   print("USAGE: python3 step0_setup.py refprefix refsheet seizuresheet ivory_paths")
+  print("refprefix is a NEW prefix for this reference release") 
+  print("refsheet is data pulled from the Master spreadsheet tab 'Reference Stats'")
+  print("seizuresheet is data pulled from the Master spreadsheet tab 'Ivory Stats'")
   exit(-1)
 
 refprefix = sys.argv[1]
 refsheet = sys.argv[2]
 seizuresheet = sys.argv[3]
 pathsfile = sys.argv[4]
+pathdir = iv.readivorypath(pathsfile)
+ivory_dir = pathdir["ivory_pipeline_dir"][0]
 
 # fammatch archive; this must be done first as make_reference assumes it is done
 
 newarchive = "elephant_archive_" + refprefix
-command = ["python3","make_new_archive.py",pathsfile,newarchive]
+progname = ivory_dir + "make_new_archive.py"
+command = ["python3",progname,pathsfile,newarchive]
 iv.run_and_report(command,"Unable to make fammatch archive")
 
 # reference files
-
-command = ["python3","make_reference.py",refprefix,pathsfile]
+progname = ivory_dir + "make_reference.py"
+command = ["python3",progname,refprefix,pathsfile]
 iv.run_and_report(command,"Unable to make reference data files")
 
 # seizure raw data files
 
-command = ["python3","make_raw_seizure.py",seizuresheet]
+progname = ivory_dir + "make_raw_seizure.py"
+command = ["python3",progname,seizuresheet]
 iv.run_and_report(command,"Unable to make seizure data files")
 
